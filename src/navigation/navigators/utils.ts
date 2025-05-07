ede5e981b377e7644c7d137155fe765f09a96e98
Vasil Kostin <vasyl.kostin@flip.shop>
2025-05-07 19:36:37 +0200

    fix: types screen entry

--- src/navigation/navigators/utils.ts
+++ src/navigation/navigators/utils.ts
@@ -1,11 +1,19 @@
+import { ComponentType } from 'react';
+
 import withScreen from '@navigation/withScreen';
 
+type ScreenEntry<T> = {
+  [K in keyof T]: [
+    K,
+    T[K] extends { screen: ComponentType<infer P> } ? ComponentType<P> : never,
+  ];
+}[keyof T];
+
 export const createScreenEntries = <
-  T extends Record<string, { screen: React.ComponentType<any> }>,
+  T extends Record<string, { screen: ComponentType<any> }>,
 >(
   screens: T,
-): [keyof T, React.ComponentType<any>][] =>
+): ScreenEntry<T>[] =>
   Object.entries(screens).map(
-    ([name, { screen }]) =>
-      [name, withScreen(screen)] as [keyof T, React.ComponentType<any>],
+    ([name, { screen }]) => [name, withScreen(screen)] as ScreenEntry<T>,
   );
